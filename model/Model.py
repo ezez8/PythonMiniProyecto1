9231199c3faa0fde8fea10576f95965767e6cee8
Yeisson Venencia <yvenencia.18@est.ucab.edu.ve>
2020-11-19 00:19:14 -0400

    PP-001: creacion de modelo y entidad para almacenamiento de ordenes

--- model/Model.py
+++ model/Model.py
@@ -1,2 +1,32 @@
+from order import Order
+from sandwich import Sandwich
+from size import Size
 class Model(object):
-    pass
+    
+    def __init__(self):
+        self.__available_ingredients = []
+        self.__available_sizes = []
+        self.__order = Order()
+        self.__current_sandwich = None
+        self.__current_ingredient = None
+    
+    def get_available_ingredients(self):
+        return self.__available_ingredients
+    
+    def get_available_sizes(self):
+        return self.__available_sizes
+
+    def load_available_ingredients(self):
+        pass
+
+    def load_availables_sizes(self):
+        pass
+
+    def obtain_order_amount(self):
+        return self.__order.calculate_order_price()
+    
+    def add_sandwich_to_order(self, sandwich : Sandwich):
+        self.__order.add_sandwich(sandwich)
+    
+    def make_sandwich(self, size : Size, ingredients : list):
+        pass
---
+++ model/order.py
@@ -0,0 +1,24 @@
+from Sandwich import Sandwich
+
+class Order(object):
+
+    def __init__(self):
+        self.__sandwiches = []
+    
+    def get_sandwiches(self):
+        return self.__sandwiches
+    
+    def set_sandwiches(self, sandwiches : list):
+        self.__sandwiches = sandwiches
+    
+    def add_sandwich(self, sandwich : Sandwich):
+        self.__sandwiches.append(sandwich)
+    
+    def remove_sandwich(self, id : int):
+        pass
+
+    def calculate_order_price(self):
+        price = 0
+        for sandwich in self.__sandwiches:
+            price += sandwich.calculate_price()
+        return price
