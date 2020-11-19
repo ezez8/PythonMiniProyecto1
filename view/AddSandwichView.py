3b7765dd30c0abecdd40a7016c0361d5b539b24d
Yeisson Venencia <yvenencia.18@est.ucab.edu.ve>
2020-11-19 15:38:02 -0400

    PP-002: Creacion de vista para agregar sandwich

---
+++ view/AddSandwichView.py
@@ -0,0 +1,29 @@
+from view.View import View
+
+class AddSandwichView(View):
+    def __init__(self, ingredient_options: dict, size_options: dict):
+        self.size_options = size_options
+        super().__init__(options)
+    
+    def display_size_options(self):
+        print('Tamaños :')
+        for command,option in self.size_options.items():
+            print(f'( {command} )    {option}')
+
+    def display_main_message(self):
+        print('**************************')
+        print('*     SANDWICHES UCAB    *')
+        print('**************************\n')
+        print('Creacion de sandwich\n')
+    
+    def start_display(self):
+        self.display_main_message()
+        self.display_size_options()
+        self.display_request_message()
+
+    def display_request_message(self):
+        print('Indique una opcion para continuar: ', end='')
+
+    def display_error_message(self):
+        print('=> Debe ingresar una opcion valida')
+    
