3f1b9699e89140c0ab3e42cf21af7592f58ed351
Roberto Carbajales <robertocd98@gmail.com>
2020-11-20 13:38:13 -0400

    agregando funcion de clonar sandwich

--- controller/Controller.py
+++ controller/Controller.py
@@ -1,5 +1,5 @@
 from model.Model import Model
-from view import View,WelcomeView,OrderView,AddSandwichView
+from view import View,WelcomeView,OrderView,AddSandwichView,CloneSandwichView
 from sys import exit
 
 class Controller(object):
@@ -68,7 +68,7 @@ class Controller(object):
         elif user_input == 'e':
             self.welcome()
         elif user_input == 'c':
-            self.welcome()
+            self.clone_sandwich()
         elif user_input == 'm':
             self.welcome()
         elif user_input == 'p':
@@ -105,6 +105,38 @@ class Controller(object):
         self.view.display_finish_message()
         input()
         self.order_menu()
+
+    def clone_sandwich(self):
+        order = self.model.get_order()
+        self.__initiate_view(CloneSandwichView.CloneSandwichView(order))
+        self.view.display_request_message()
+        user_want_clone = True
+
+        while user_want_clone:
+            selected_option = input()
+            cont = len(self.model.get_order().get_sandwiches())
+            if int(selected_option) > cont or int(selected_option) < 0:
+                self.view.display_error_message()
+            else:
+                sandwich = self.model.get_order().get_sandwiches()[int(selected_option) - 1]
+                self.model.add_cloned_sandwich_to_order(sandwich)
+                user_want_clone = False
+        self.view.display_finish_message()
+        input()
+        self.order_menu()
+
+                
+                
+                
+
+
+
+                    
+                
+
+
+        
+        
 
     def end_program(self):
         exit()
