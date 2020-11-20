d0fd3447fa82cc026c073fa6843112c29f8e78bc
yeisson-venencia <63071952+yeisson-venencia@users.noreply.github.com>
2020-11-20 12:39:48 -0400

    PP-004: Reordenamiento en el vista de creacion de sandwich (#4)

--- model/Sandwich.py
+++ model/Sandwich.py
@@ -33,3 +33,23 @@ class Sandwich(object):
         for ingredient in self.ingredients_list:
             price += ingredient.price
         return price
+    
+    def get_small_description(self):
+        return self.size.name
+
+    def get_full_description(self):
+        size_section = self.size.name
+
+        ingredients_list = [ingredient.name for ingredient in self.ingredients_list]
+        number_of_ingredients = len(ingredients_list)
+        ingredients_section = ''
+        if  number_of_ingredients == 0:
+            ingredients_section = ''
+        elif number_of_ingredients == 1:
+            ingredients_section = f' con {ingredients_list[0]}'
+        else:
+            first_part = ', '.join(ingredients_list[0:-1])
+            last_part = ingredients_list[-1]
+            ingredients_section = f' con {first_part} y {last_part}'
+        
+        return f'{size_section}{ingredients_section}'
